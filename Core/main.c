f8aec4952379ea88bd544a2c97602cc1f8e613db
M. Sari Masri <sari@private.de>
2024-08-08 18:54:17 +0200

    basic version

---
+++ Core/main.c
@@ -0,0 +1,56 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <stdint.h>
+
+/* Private includes ----------------------------------------------------------*/
+
+/* Private typedef -----------------------------------------------------------*/
+
+/* Private define ------------------------------------------------------------*/
+
+/* Private macro -------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+
+/**
+ * @brief  The application entry point.
+ * @retval int */
+int main(void)
+{
+
+	/* MCU Configuration--------------------------------------------------------*/
+//	__disable_irq();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+
+
+	/* Configure the system clock */
+
+	/* Initialize all configured peripherals */
+
+//	__enable_irq();
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1)
+	{
+
+	}
+}
+
