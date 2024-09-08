95a6dce2a84be030b0d28f7728be1d91237c5d0e
M. Sari Masri <sari@example.de>
2024-09-08 20:21:27 +0200

    fixed clock bug

--- Core/Tasks/SystemClockConfigurtion/ConfigSystemClock.c
+++ Core/Tasks/SystemClockConfigurtion/ConfigSystemClock.c
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * @file    ConfigSystemClock.c
  * @author  sari
- * @email   
- * @website 
+ * @email
+ * @website
  * @date    Aug 18, 2024
- *          
- * @brief   
- * @note    
+ *
+ * @brief
+ * @note
  *
 @verbatim
 Copyright (C) on GitHub msmasri/stm32project, 2024
@@ -41,84 +41,72 @@ static void InitSystemClock(void);
 static void InitBusesClock(void);
 /* External variables --------------------------------------------------------*/
 
-
-
-
 /*
  * @brief: configure PLL with 180MHz as system clock
  * @note:
  * @retval none
  * */
-static void InitSystemClock(void)
-{
+static void InitSystemClock(void) {
+  /* disable pll if it is selected as system clock */
+  RCC->CR &= ~RCC_CR_PLLON;
 
-	/* disable pll if it is selected as system clock */
-	if (RCC->CFGR & RCC_CFGR_SWS_PLL)
-	{
-		RCC->CR &= ~RCC_CR_PLLON;
-	};
+  /* select HSI as clock input for PLL */
+  RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSI;
 
-	/* select HSI as clock input for PLL */
-	RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSI;
+  /* start configuring pll calculated values */
+  // TODO: could make function to select the best values automatically?
+  /* to get 180MHz as output it should be chosen as follows
+   * PLLN = 360 = 0x168
+   * PLLM = 16 = 0x10
+   * PPLP = 2 = 0x2
+   * */
+  RCC->PLLCFGR &= ~(RCC_PLLCFGR_PLLN);  // Reset all old configuration
+  RCC->PLLCFGR |= (RCC_PLLCFGR_PLLN_3 | RCC_PLLCFGR_PLLN_5 |
+                   RCC_PLLCFGR_PLLN_6 | RCC_PLLCFGR_PLLN_8);
+  // RCC->PLLCFGR |= (360 << RCC_PLLCFGR_PLLN_Pos);
 
-	/* start configuring pll calculated values */
-	//TODO: could make function to select the best values automatically?
-	/* to get 180MHz as output it should be chosen as follows
-	 * PLLN = 360 = 0x168
-	 * PLLM = 16 = 0x10
-	 * PPLP = 2 = 0x2
-	 * */
-	RCC->PLLCFGR &= ~(RCC_PLLCFGR_PLLN); //Reset all old configuration
-	RCC->PLLCFGR |= (RCC_PLLCFGR_PLLN_3 | RCC_PLLCFGR_PLLN_5 | RCC_PLLCFGR_PLLN_6
-			| RCC_PLLCFGR_PLLN_8);
-//	RCC->PLLCFGR &= ~(RCC_PLLCFGR_PLLN_7);
+  RCC->PLLCFGR &= ~RCC_PLLCFGR_PLLM;  // Reset all old configuration
+  RCC->PLLCFGR |= RCC_PLLCFGR_PLLM_4;
+  // RCC->PLLCFGR |= 16 << RCC_PLLCFGR_PLLM_Pos;
 
-	RCC->PLLCFGR &= ~RCC_PLLCFGR_PLLM; //Reset all old configuration
-	RCC->PLLCFGR |= (RCC_PLLCFGR_PLLM_1 | RCC_PLLCFGR_PLLM_3);
+  RCC->PLLCFGR &= ~RCC_PLLCFGR_PLLP;  // Reset all old configuration
 
-	RCC->PLLCFGR &= ~RCC_PLLCFGR_PLLP; //Reset all old configuration
-	RCC->PLLCFGR |= RCC_PLLCFGR_PLLP_1;
+  /* turn PLL on */
+  RCC->CR |= RCC_CR_PLLON;
 
-	/* select the division factor for I2Ss, SAIs, SYSTEM and SPDIF-Rx clocks
-	 * driven by PLL clock */
-	RCC->PLLCFGR |= RCC_PLLCFGR_PLLR_1; // divided by 2
+  /* wait until PLL ready */
+  while (RCC->CR & RCC_CR_PLLRDY) {
+  };
 
-	/* division factor for USB OTG FS, SDIOclocks */
-	RCC->PLLCFGR |= RCC_PLLCFGR_PLLQ_2; // divided by 2
+  /* switch to PLL*/
+  RCC->CFGR |= RCC_CFGR_SW_HSI;
+  RCC->CFGR |= RCC_CFGR_SW_PLL;
 
-	/* turn PLL on */
-	RCC->CR |= RCC_CR_PLLON;
-
-	/* wait until PLL ready */
-	while (RCC->CR & RCC_CR_PLLRDY)
-	{
-	};
-	
-	/* switch to PLL*/
-	RCC->CFGR |= RCC_CFGR_SW_PLL;
-
-	/* wait until switching to PLL */
-	while (RCC->CFGR & RCC_CFGR_SWS_PLL)
-	{
-	};
-
+  /* wait until switching to PLL */
+  while (RCC->CFGR & RCC_CFGR_SWS_PLL) {
+  };
 }
 
-static void InitBusesClock(void)
-{
-	/* AHB bus */
-//	RCC->CFGR |= RCC_CFGR_HPRE_DIV2;
+static void InitBusesClock(void) {
+  /* select the division factor for I2Ss, SAIs, SYSTEM and SPDIF-Rx clocks
+   * driven by PLL clock */
+  RCC->PLLCFGR |= RCC_PLLCFGR_PLLR_1;  // divided by 2
 
-	/* PPRE1 bus AHB clock divided by 4 to get the maximum with 45MHz */
-	RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;
+  /* division factor for USB OTG FS, SDIOclocks */
+  RCC->PLLCFGR |= RCC_PLLCFGR_PLLQ_2;  // divided by 2
+  /* AHB bus, system clock divided by 4 */
+  RCC->CFGR |= RCC_CFGR_HPRE_DIV4;
 
-	/* PPRE2 (high speed) bus AHB clock divided by 2 to get the maximum with 90MHz*/
-	RCC->CFGR |= RCC_CFGR_PPRE2_DIV2;
+  /* PPRE1 bus AHB clock divided by 4 to get the maximum with 45MHz, if pll is
+   * used otherwise, the HSI is used with 16MHz. so 8MHz */
+  RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
+
+  /* PPRE2 (high speed) bus AHB clock divided by 2 to get the maximum with 90MHz
+   * if pll is used otherwise, the HSI is used with 16MHz. so 8MHz */
+  RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;
 }
 
-
-void InitMainSysClock(void)
-{
-	InitSystemClock();
-	InitBusesClock();
+void InitMainSysClock(void) {
+  InitSystemClock();
+  InitBusesClock();
 }
